ab4aeb532b85214ec88134fa8a88ced65c1d51da
Fahmi Noh <fahminoh.dev@gmail.com>
2024-01-15 11:07:13 +0800

    first commit

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            
+            //admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => '1',
+            ],
+            
+            //instructor
+            [
+                'name' => 'Dr Oppie',
+                'username' => 'Oppie',
+                'email' => 'droppie@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'instructor',
+                'status' => '1',
+            ],
+
+            //user
+            [
+                'name' => 'Harvey',
+                'username' => 'harvey',
+                'email' => 'harvey@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => '1',
+            ],
+
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+//Admin Group Middleware
+Route::middleware(['auth','roles:admin'])->group(function(){
+
+
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+}); //End of admin group middleware
+
+
+//Instructor Group Middleware
+Route::middleware(['auth','roles:instructor'])->group(function(){
+
+Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
+
+}); //End of instructor group middleware
