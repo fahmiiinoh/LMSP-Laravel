3cf91622f385abcfaca37dc1a992b3d9cd417042
Fahmi Noh <fahminoh.dev@gmail.com>
2024-02-21 15:18:46 +0800

    minor v2

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -67,7 +67,7 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6">
-                    <p class="copy-desc">&copy; 2021 Aduca. All Rights Reserved. by <a href="https://techydevs.com/">TechyDevs</a></p>
+                    <p class="copy-desc">&copy; 2024 LMSP. All Rights Reserved. by <a href="https://fahminoh.netlify.app/">FN</a></p>
                 </div><!-- end col-lg-6 -->
                 <div class="col-lg-6">
                     <div class="d-flex flex-wrap align-items-center justify-content-end">
